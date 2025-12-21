da6e26ae69cda1c5e635d9fda68acfe5dca46dd5
taro <asa.umareru.2@gmail.com>
2025-12-21 15:19:01 +0900

    三回目コミット

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controller\TodoController;
+use App\Http\Controllers\TodoController;
+use App\Http\Controllers\CategoryController;
 
-Route::get('/',[TodoController::class,'index']);
-Route::post('/todos',[TodoController::class,'store']);
+
+Route::get('/', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
 Route::patch('/todos/update', [TodoController::class, 'update']);
-Route::delete('/todos/delete', [TodoController::class, 'destroy']);
+Route::delete('/todos/delete', [TodoController::class, 'destroy'])
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::patch('/categories/update', [CategoryController::class, 'update']);
+
 
 
 /*
